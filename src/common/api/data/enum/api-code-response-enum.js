a2dd4197727fe7d4a2e6130a8e62516c0789fbf5
Mouslimov-A <mouslimov.aslan@gmail.com>
2025-11-25 23:27:24 +0100

    delete

--- src/common/api/data/enum/api-code-response-enum.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ApiCodeResponse = void 0;
-var ApiCodeResponse;
-(function (ApiCodeResponse) {
-    ApiCodeResponse["TEST"] = "api.result.test";
-})(ApiCodeResponse || (exports.ApiCodeResponse = ApiCodeResponse = {}));
