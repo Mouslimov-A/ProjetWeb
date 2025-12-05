d610095803f3e3fe12c5ea6c699209ca64c8c16f
Mouslimov-A <mouslimov.aslan@gmail.com>
2025-12-05 18:14:01 +0100

    Projet web

--- ExempleApp/src/app/security/page/sign-in-page/sign-in-page.ts
+++ ExempleApp/src/app/security/page/sign-in-page/sign-in-page.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
-import {FloatInput} from '../../../shared/ui/form/component/float-input/float-input';
-import {FloatInputConfig} from '../../../shared/ui/form/model/float-input.config';
-
+import { FloatInput } from '../../../shared/ui/form/component/float-input/float-input';
+import { FloatInputConfig } from '../../../shared/ui/form/model/float-input.config';
 
 @Component({
   selector: 'app-sign-in-page',
@@ -13,10 +12,24 @@ import {FloatInputConfig} from '../../../shared/ui/form/model/float-input.config
   styleUrl: './sign-in-page.scss',
 })
 export class SignInPage {
+
   protected passwordConfig: FloatInputConfig = {
     type: 'password', label: 'password', defaultValue: ''
   }
+
   protected usernameConfig: FloatInputConfig = {
-    type: 'text', label: 'username', defaultValue:'m.aslan@gmail.com'
+    type: 'text', label: 'username', defaultValue: 'm.aslan@gmail.com'
+  }
+
+  onClickEvent(event: FloatInputConfig): void {
+    console.log('Event reçu du composant enfant :', event);
+  }
+  onClick(event: MouseEvent): void {
+    this.usernameConfig.label = "Nouveau Label";
+  }
+  changeLabel(event: MouseEvent): void {
+    console.log('Bouton cliqué !');
+
+    this.usernameConfig = { ...this.usernameConfig, label: 'Nouvel Email' };
   }
 }
